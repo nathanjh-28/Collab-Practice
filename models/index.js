7e92f7dc5f4512f51b9eefc7953fcd336899f9b1
jimmyspann <jimmyleespann@outlook.com>
2020-07-09 15:20:08 -0400

    Added model's index file

--- models/index.js
+++ models/index.js
@@ -1,2 +1,18 @@
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/express-blog-12';
 
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+
+// Make All Models Available
+module.exports = {
+  Author: require('./Author'),
+  Article: require('./Article'),
+};
